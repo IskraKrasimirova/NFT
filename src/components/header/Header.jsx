d39496b9f9bcd67722f106c173c6524b14e7a8ed
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2023-02-20 15:45:27 +0200

    Edit header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,22 +9,21 @@ import Button from "@mui/material/Button";
 
 export default function Header() {
     return (
-        <div className={classNames(styles.wrapper)}>
-            <Grid Container className={classNames(styles.container)} maxWidth="xl">
-                <Grid item xs={2}>
-                    <Logo />
-                </Grid>
-                <Grid item xs={6}>
-                    <SearchIcon />
-                    <InputBase className={classNames(styles.searchField)} placeholder="Find assets, users and activity..." />
-                </Grid>
-                <Grid item xs={4} className={classNames(styles.buttonContainer)}>
-                    <ButtonBase variant="contained" color="primary" className={classNames(styles.buttonBase)}>Home</ButtonBase>
-                    <ButtonBase variant="contained" color="primary" className={classNames(styles.buttonBase)}>Activity</ButtonBase>
-                    <Button variant="contained" color="primary" className={classNames(styles.button)}>EXPLORE</Button>
-                </Grid>
+        <Grid Container className={classNames(styles.container)} maxWidth="xl">
+            <Grid item xs={2}>
+                <Logo />
             </Grid>
-        </div>
-
+            <Grid item xs={6}>
+                <div className={classNames(styles.searchContainer)}>
+                    <SearchIcon />
+                    <InputBase className={classNames(styles.searchField)} placeholder="Find assets, users and activity" />
+                </div>
+            </Grid>
+            <Grid item xs={4} className={classNames(styles.buttonContainer)}>
+                <ButtonBase variant="contained" color="primary" className={classNames(styles.buttonBase)}>Home</ButtonBase>
+                <ButtonBase variant="contained" color="primary" className={classNames(styles.buttonBase)}>Activity</ButtonBase>
+                <Button variant="contained" color="primary" className={classNames(styles.button)}>EXPLORE</Button>
+            </Grid>
+        </Grid>
     );
 }
